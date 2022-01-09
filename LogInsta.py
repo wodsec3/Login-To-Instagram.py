65637295b9ec7772ee55f172c70a0947f932b30c
mammad shafiei <96043653+wodsec3@users.noreply.github.com>
2022-01-09 13:42:56 +0330

    Update LogInsta.py

--- LogInsta.py
+++ LogInsta.py
@@ -1,22 +1,27 @@
+# pip install my_fake_useragent
+# pip install requests
+
 import requests, json, urllib.parse
 from my_fake_useragent import UserAgent as u
 
 username = input('username: ')
 password = input('Password: ')
 
-
+# Create a random User-Agent Object
 ua=u(family='chrome')
 hd=ua.random()
 x='User-Agent'
 hd1={x:f'{hd}'}
 hd2=(f'{x}: {hd}')
 
-
+# Create a requests Object
 r = requests.Session()
 res = r.get('https://www.instagram.com/', headers=hd1)
 
+# Create Payloads For Post to Instagram.com
 payload = {'username':username,'enc_password':'#PWD_INSTAGRAM_BROWSER:0:1254625879:'+password,'queryParams':'{}','optIntoOneTap':'false'}
 
+# the text headers
 headers_text = '''Host: www.instagram.com
 %s
 Accept: */*
@@ -34,8 +39,9 @@ TE: Trailers'''%(hd2,res.cookies['csrftoken'],str(len(urllib.parse.urlencode(pay
 
 payload_headers = {i.split(': ')[0]:i.split(': ')[1] for i in headers_text.split('\n')}
 
+# post headers And data to login URL . . .
 resp = r.post("https://www.instagram.com/accounts/login/ajax/", headers=payload_headers,data=payload)
-
+# Result .
 if json.loads(resp.text)['authenticated'] == True:
 	print('[+] Login successfully!')
 	print(json.loads(resp.text))
